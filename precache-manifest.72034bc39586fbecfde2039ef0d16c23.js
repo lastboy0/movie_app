beaba9e5e6c3c2c4d9539a32a387c83c91531794
lastboy <lastboy0@gmail.com>
2020-02-09 14:38:08 +0900

    Updates

---
+++ precache-manifest.72034bc39586fbecfde2039ef0d16c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621a325909df9e313245c40bd0553862",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "96309ca562785deb2b8e",
+    "url": "/movie_app/static/css/main.e4b085b0.chunk.css"
+  },
+  {
+    "revision": "70f0c6002f4486a8ba63",
+    "url": "/movie_app/static/js/2.2d8a139a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.2d8a139a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96309ca562785deb2b8e",
+    "url": "/movie_app/static/js/main.bf351836.chunk.js"
+  },
+  {
+    "revision": "afdf0258d99bd56019d8",
+    "url": "/movie_app/static/js/runtime-main.dd5bb62c.js"
+  }
+]);
